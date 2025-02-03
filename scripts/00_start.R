1da18882dfe6589a47ce9f6da163c244f6df9abe
Carlijn van der Weerd <c.van.der.weerd@student.rug.nl>
2025-02-03 10:07:26 +0100

    start of new repository

---
+++ scripts/00_start.R
@@ -0,0 +1,15 @@
+### File for new package installations 
+install.packages("renv")
+renv::init()
+
+
+install.packages('ape',dependencies = TRUE)
+install.packages('phytools',dependencies = TRUE)
+install.packages('DAISIE',dependencies = TRUE)
+install.packages('DAISIEprep',dependencies = TRUE)
+install.packages("paran")
+install.packages("splits", repos="http://R-Forge.R-project.org")
+install.packages("tidyverse")
+
+
+                 
